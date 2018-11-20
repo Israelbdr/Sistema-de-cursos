dec3063247a37878e3272028084f730b1bbd295a
Israel Reis <israelbdr@icloud.com>
2018-11-20 13:49:19 -0300

    Updated menu.py
    Now you can exhibit both tables by choosing them on the ComboBox and clicking in Consultar

--- menu.py
+++ menu.py
@@ -44,17 +44,17 @@ class Ui_menu(object):
         self.consulta_button = QtGui.QPushButton(self.centralwidget)
         self.consulta_button.setGeometry(QtCore.QRect(20, 30, 90, 30))
         self.consulta_button.setObjectName(_fromUtf8("consulta_button"))
-        self.consulta_button.clicked.connect(self.mostrarTabelaAlunos)
+        self.consulta_button.clicked.connect(self.mostrarTabela)
         self.tableWidget.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-        # self.tableWidget.customContextMenuRequested.connect(self.menuContextual)
 
         self.colunas_box = QtGui.QComboBox(self.centralwidget)
         self.colunas_box.setGeometry(QtCore.QRect(130, 30, 211, 31))
         self.colunas_box.setEditable(False)
         self.colunas_box.setObjectName(_fromUtf8("colunas_box"))
-        self.colunas_box.insertItem(0, "Alunos")
-        self.colunas_box.insertItem(1, "Cursos")
-        self.colunas_box.currentText()
+
+        self.colunas_box.addItem("Alunos")
+        self.colunas_box.addItem("Cursos")
+
         self.apagar_button = QtGui.QPushButton(self.centralwidget)
         self.apagar_button.setGeometry(QtCore.QRect(610, 30, 120, 30))
         self.apagar_button.setObjectName(_fromUtf8("apagar_button"))
@@ -66,7 +66,7 @@ class Ui_menu(object):
         QtCore.QMetaObject.connectSlotsByName(menu)
 
 
-    #Função para limpar a tabela de suas entradas
+    #Função para limpar as entradas na tabela.
     def apagarTabela(self):
         self.tableWidget.clearContents()
         self.tableWidget.setRowCount(0)
@@ -76,27 +76,16 @@ class Ui_menu(object):
         self.consulta_button.setText(_translate("menu", "Consultar", None))
         self.apagar_button.setText(_translate("menu", "Limpar tela", None))
 
-    # def menuContextual(self, posicao):
-    #     arrayDados = self.tableWidget.selectecIndexes()
-    #     if arrayDados:
-    #         menu = QMenu()
-    #         iGrupo = QtGui.QActionGroup(self)
-    #         iGrupo.setExclusive(True)
-    #         menu.addAction(QtGui.QAction("Copiar tudo", iGrupo))
-    #         colunas = [self.tableWidget.horizontalHeaderItem(coluna).text(),
-    #                    for coluna in range(self.tableWidget.columnCount())
-    #                    if not self.tableWidget.isColumnHidden(coluna)]
-    #
-
     # Função para mostrar a tabela.
-    def mostrarTabelaAlunos(self):
+    def mostrarTabela(self):
         import sqlite3
         db = sqlite3.connect("Database.db")
         cursor = db.cursor()
 
         # Função pra setar qual tabela será exibida.
-        if self.colunas_box.currentText == "Alunos":
+        if self.colunas_box.currentText() == "Alunos":
             result = cursor.execute("SELECT * FROM alunos;")
+            self.tableWidget.setColumnCount(7)
             nomeColunas = ("CPD", "Nome do aluno", "Telefone", "Endereço", "CEP", "Email", "Código do curso")
             self.tableWidget.setHorizontalHeaderLabels(nomeColunas)
             self.tableWidget.setColumnCount(7)
@@ -114,6 +103,7 @@ class Ui_menu(object):
                 row += 1
         else:
             result = cursor.execute("SELECT * FROM cursos;")
+            self.tableWidget.setColumnCount(4)
             nomeColunas = ("Código do Curso", "Nome do Curso", "Carga Horária", "Data de cadastro")
             self.tableWidget.setHorizontalHeaderLabels(nomeColunas)
             self.tableWidget.setColumnCount(4)
@@ -126,3 +116,7 @@ class Ui_menu(object):
                 self.tableWidget.setItem(row, 2, QtGui.QTableWidgetItem(str(arrayDados[2])))
                 self.tableWidget.setItem(row, 3, QtGui.QTableWidgetItem(str(arrayDados[3])))
                 row += 1
+        db.commit()
+        db.close()
+
+
